6fc5f311f1fecd711526eb74a6638b6dab0e4694
Robert <smithre2@grizzlies.adams.edu>
2018-07-26 10:13:56 -0600

    Cleaning up

--- RaspBerryPi/Client.py
+++ RaspBerryPi/Client.py
@@ -1,11 +1,70 @@
 '''
 '
-'USE WITH PYTHON 2.7
+'USE WITH PYTHON 3
 '
 '''
 
 import socket
 import sys
+import SpeechRecognition as sr
+
+
+
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+#creating voice recognizer and microphone
+recognizer = sr.Recognizer()
+
+#use sr.Microphone.list_microphone_names() to find the device index
+#if device_index arg is omitted, it will use the default microphone
+microphone = sr.Microphone()
+
 
 ip_address = input("Enter the IP address of the server: ")
 # Create a TCP/IP socket
@@ -16,8 +75,9 @@ while True:
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(server_address)
-        message=input('Message: ')
-        message = message.encode()
+        #message=input('Message: ')
+        response = recognize_speech_from_mic(recognizer, microphone)
+        message = response.encode()
         if message=='quit':
             break
         sock.send(message)
